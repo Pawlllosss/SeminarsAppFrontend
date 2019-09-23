23a9715424352fe50172fdaaff6a86c8c1c63ae5
Pawlllosss <oczadlypawe@gmail.com>
2019-09-23 22:54:45 +0200

    + project draft + fetching available courses

---
+++ src/course/AvailableCourses.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+import Course from "./Course";
+
+class AvailableCourses extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            courses: []
+        };
+
+        this.apiBasePath = 'http://seminars-api.herokuapp.com/';
+        this.coursePath ='course/';
+    }
+
+    componentDidMount() {
+        axios.get(this.apiBasePath + this.coursePath)
+            .then(response => this.setState({courses : response.data._embedded.courses}));
+    }
+
+    render() {
+        const courses = this.state.courses;
+        const courseNodes = courses.map(course => <Course course={course} key={course._links}/>);
+        console.log(courseNodes);
+
+        return (
+          <div className="AvailableCourses">
+              <ul>{courseNodes}</ul>
+          </div>
+        );
+    }
+}
+
+export default AvailableCourses;
---
+++ src/course/Course.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Course({course}) {
+    return(<li>{course.name}</li>)
+}
+
+export default Course;
