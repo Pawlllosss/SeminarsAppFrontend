87a4de93ae7e6d0f53b11916c4caf265a05c33bc
Pawlllosss <oczadlypawe@gmail.com>
2019-11-24 19:37:07 +0100

    + retrieving userVotes

--- src/components/seminar/user/SeminarsUserView.js
+++ src/components/seminar/user/SeminarsUserView.js
@@ -1,10 +1,27 @@
 import React, {Fragment} from 'react';
 import {API_URL} from "../../../config";
 import axios from "axios";
-import {COURSE_API_PATH, COURSE_CURRENT_USER_PATH,} from "../../course/CourseConstants";
-import {Checkbox, Grid, List, ListItem, ListItemIcon, ListItemText, Paper, Typography} from '@material-ui/core';
+import {
+    COURSE_API_PATH,
+    COURSE_CURRENT_USER_PATH
+} from "../../course/CourseConstants";
+import {
+    Checkbox,
+    Grid, IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemSecondaryAction,
+    ListItemText,
+    Paper,
+    Typography
+} from '@material-ui/core';
 import getAuthorizationBearerHeader from "../../../utils/authentication/BearerTokenSetter";
 import {getHumanReadableDate} from "../SeminarUtils";
+import {EditOutlined as EditIcon,
+    ExpandLess as Up,
+    ExpandMore as Down} from "@material-ui/icons";
+import {Link} from "react-router-dom";
 
 class SeminarsUserView extends React.Component {
 
@@ -15,7 +32,8 @@ class SeminarsUserView extends React.Component {
             checkedCourse: "",
             topics: [],
             checkedTopic: "",
-            seminars: []
+            seminars: [],
+            votes: []
         };
     }
 
@@ -65,6 +83,7 @@ class SeminarsUserView extends React.Component {
                 seminars: []
             });
             this.fetchTopics(course);
+            this.fetchVotes(course);
         }
     };
 
@@ -73,6 +92,13 @@ class SeminarsUserView extends React.Component {
             .then(response => response.data)
             .then(data => data._embedded !== undefined ? data._embedded.topics : [])
             .then(topics => this.setState({topics: topics}));
+    };
+
+    fetchVotes(course) {
+        return axios.get(course._links.userVotes.href, {headers: getAuthorizationBearerHeader()})
+            .then(response => response.data)
+            .then(data => data.seminars !== undefined ? data.seminars : [])
+            .then(votes => this.setState({votes: votes}));
     };
 
     isCourseChecked(course) {
@@ -137,38 +163,82 @@ class SeminarsUserView extends React.Component {
         ));
     }
 
+    getUserVotes() {
+        const votes = this.state.votes;
+
+        return votes.map(vote => (
+            <ListItem key={vote.id}>
+                <ListItemText
+                    primary={vote.topicName + " " + getHumanReadableDate(vote.date)}
+                />
+                <ListItemSecondaryAction>
+                    <IconButton
+                        color="inherit"
+                        component={Link}
+                        onClick={this.handleClickUp(vote)}
+                    >
+                        <Up/>
+                    </IconButton>
+                    <IconButton
+                        color="inherit"
+                        component={Link}
+                        onClick={this.handleClickDown(vote)}
+                    >
+                        <Down />
+                    </IconButton>
+                </ListItemSecondaryAction>
+            </ListItem>
+        ));
+    }
+
+    handleClickUp = (vote) => () => {
+    };
+
+
+    handleClickDown = (vote) => () => {
+    };
+
     render() {
         const courseNodes = this.getCourseNodes();
         const topicNodes = this.getTopicNodes();
         const seminarNodes = this.getSeminarNodes();
+        const userVotes = this.getUserVotes();
 
         return (
-            <Grid container spacing={2} justify="center" alignItems="center">
-                <Grid item xs={3}>
-                    <Fragment>
-                        <Typography variant="h6">Available Courses</Typography>
-                        <Paper elevation={1}>
-                            <List>{courseNodes}</List>
-                        </Paper>
-                    </Fragment>
+            <div className='seminars-user'>
+                <Grid container spacing={2} justify="center" alignItems="center">
+                    <Grid item xs={3}>
+                        <Fragment>
+                            <Typography variant="h6">Available Courses</Typography>
+                            <Paper elevation={1}>
+                                <List>{courseNodes}</List>
+                            </Paper>
+                        </Fragment>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Fragment>
+                            <Typography variant="h6">Topics</Typography>
+                            <Paper elevation={1}>
+                                <List>{topicNodes}</List>
+                            </Paper>
+                        </Fragment>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Fragment>
+                            <Typography variant="h6">Seminars</Typography>
+                            <Paper elevation={1}>
+                                <List>{seminarNodes}</List>
+                            </Paper>
+                        </Fragment>
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                    <Fragment>
-                        <Typography variant="h6">Topics</Typography>
-                        <Paper elevation={1}>
-                            <List>{topicNodes}</List>
-                        </Paper>
-                    </Fragment>
-                </Grid>
-                <Grid item xs={3}>
-                    <Fragment>
-                        <Typography variant="h6">Seminars</Typography>
-                        <Paper elevation={1}>
-                            <List>{seminarNodes}</List>
-                        </Paper>
-                    </Fragment>
-                </Grid>
-            </Grid>
+                <Fragment>
+                    <Typography variant="h6">User votes for course</Typography>
+                    <Paper elevation={1}>
+                        <List>{userVotes}</List>
+                    </Paper>
+                </Fragment>
+            </div>
         );
     }
 }
