821e08efb0e91d8b81c72e604e1640452ef056d7
Pawlllosss <oczadlypawe@gmail.com>
2019-12-09 00:56:23 +0100

    # improved sign up form # fixed auto focus in forms

--- src/components/authentication/SignUpForm.js
+++ src/components/authentication/SignUpForm.js
@@ -2,18 +2,31 @@ import React from "react";
 import {connect} from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import {signUpAction} from "../../_actions/authentication/SignUpAction";
+import {Button, TextField, Typography} from "@material-ui/core";
 
 class SignUpForm extends React.Component {
 
-    submit = (values) => {
-        this.props.signUpAction(values, this.props.history);
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            repassword: "",
+            nickname: "",
+            firstName: "",
+            lastName: ""
+        };
+    }
+
+    submit = () => {
+        this.props.signUpAction(this.state, this.props.history);
     };
 
     errorMessage() {
         if (this.props.errorMessage) {
             return (
-                <div className="info-red">
-                    {this.props.errorMessage}
+                <div className="signin-error">
+                    <Typography variant="caption" align="center">{this.props.errorMessage}</Typography>
                 </div>
             );
         }
@@ -22,42 +35,73 @@ class SignUpForm extends React.Component {
     render() {
         const {handleSubmit} = this.props;
         return (
-            <div className="form">
-                <div className="container">
-                    <h2>Sign Up</h2>
-                    <form onSubmit={ handleSubmit(this.submit) }>
-                        <Field name="email"
-                               component="input"
-                               type="text"
-                               placeholder="Email"
-                        />
-                        <Field name="password"
-                               component="input"
-                               type="password"
-                               placeholder="Password"
-                        />
-                        <Field name="repassword"
-                               component="input"
-                               type="password"
-                               placeholder="Repeat password"
-                        />
-                        <Field name="nickname"
-                               component="input"
-                               type="text"
-                               placeholder="Nickname"
-                        />
-                        <Field name="firstName"
-                               component="input"
-                               type="text"
-                               placeholder="Firstname"
-                        />
-                        <Field name="lastName"
-                               component="input"
-                               type="text"
-                               placeholder="Lastname"
-                        />
-                        <button type="submit" className="blue">Sign Up</button>
-                    </form>
+            <div className="form" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <div className="container" style={{width: "70%"}}>
+                    <Typography variant="h4" align="center">Sign In</Typography>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="email"
+                        label="Email"
+                        type="text"
+                        required={true}
+                        value={this.state.email}
+                        onChange={(event) => this.setState({email: event.target.value})}
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        id="password"
+                        label="Password"
+                        type="password"
+                        required={true}
+                        value={this.state.password}
+                        onChange={(event) => this.setState({password: event.target.value})}
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        id="repassword"
+                        label="Repeat password"
+                        type="password"
+                        required={true}
+                        value={this.state.repassword}
+                        onChange={(event) => this.setState({repassword: event.target.value})}
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        id="nickname"
+                        label="Nickname"
+                        type="text"
+                        required={true}
+                        value={this.state.nickname}
+                        onChange={(event) => this.setState({nickname: event.target.value})}
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        id="firstName"
+                        label="First name"
+                        type="text"
+                        required={true}
+                        value={this.state.firstName}
+                        onChange={(event) => this.setState({firstName: event.target.value})}
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        id="lastName"
+                        label="Last name"
+                        type="text"
+                        required={true}
+                        value={this.state.lastName}
+                        onChange={(event) => this.setState({lastName: event.target.value})}
+                        fullWidth
+                    />
+                    <Button onClick={handleSubmit(this.submit)} color="primary">
+                        Submit
+                    </Button>
                     {this.errorMessage()}
                 </div>
             </div>
