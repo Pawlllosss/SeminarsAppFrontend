4c843175ab5e7ccc4c66907c6a083ebe449f3ff1
Pawlllosss <oczadlypawe@gmail.com>
2019-12-09 00:48:13 +0100

    # improved error message displaying

--- src/components/authentication/SignInForm.js
+++ src/components/authentication/SignInForm.js
@@ -1,19 +1,28 @@
 import React from "react";
 import {connect} from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
+import {reduxForm} from 'redux-form';
 import {signInAction} from '../../_actions/authentication/SignInAction';
+import {Button, TextField, Typography} from "@material-ui/core";
 
 class SignInForm extends React.Component {
 
-    submit = (values) => {
-        this.props.signInAction(values, this.props.history);
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        };
+    }
+
+    submit = () => {
+        this.props.signInAction(this.state, this.props.history);
     };
 
     errorMessage() {
         if (this.props.errorMessage) {
             return (
-                <div className="info-red">
-                    {this.props.errorMessage}
+                <div className="signin-error">
+                    <Typography variant="caption" align="center">{this.props.errorMessage}</Typography>
                 </div>
             );
         }
@@ -22,22 +31,34 @@ class SignInForm extends React.Component {
     render() {
         const {handleSubmit} = this.props;
         return (
-            <div className="form">
-                <div className="container">
-                    <h2>Sign In</h2>
-                    <form onSubmit={ handleSubmit(this.submit) }>
-                        <Field name="email"
-                               component="input"
-                               type="text"
-                               placeholder="Email"
-                        />
-                        <Field name="password"
-                               component="input"
-                               type="password"
-                               placeholder="Password"
-                        />
-                        <button type="submit" className="blue">Sign In</button>
-                    </form>
+            <div className="form" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                <div className="container" style={{width: "70%"}}>
+                    <Typography variant="h4" align="center">Sign In</Typography>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="email"
+                        label="Email"
+                        type="text"
+                        required={true}
+                        value={this.state.email}
+                        onChange={(event) => this.setState({email: event.target.value})}
+                        fullWidth
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="password"
+                        label="Password"
+                        type="password"
+                        required={true}
+                        value={this.state.password}
+                        onChange={(event) => this.setState({password: event.target.value})}
+                        fullWidth
+                    />
+                    <Button onClick={handleSubmit(this.submit)} color="primary">
+                        Submit
+                    </Button>
                     {this.errorMessage()}
                 </div>
             </div>
