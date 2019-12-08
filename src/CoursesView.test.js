fac84024e2790931fd9fd930e996069b4617d24a
Pawlllosss <oczadlypawe@gmail.com>
2019-12-08 22:24:44 +0100

    # modified travis yml

--- src/CoursesView.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CoursesView from "./components/course/CoursesView";
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CoursesView />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
